312258382f7099a2070dd054dfa8e8c49e03af5b
Luke Stanley <luke.stanley@gmail.com>
2023-01-11 09:39:34 +0000

    Moves CSS and size tweaks

--- src/App.vue
+++ src/App.vue
@@ -43,9 +43,4 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
 </style>
